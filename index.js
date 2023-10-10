9a3514f8c5ff18553cbc12d536a66bb0d92c28f6
MD Rahi <fhrstation21@gmail.com>
2023-10-10 20:13:33 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+const categoryData = require('./productsCategory.json');
+
+app.get('/', (req, res) => {
+    res.send(`Server Started On Port ${port}`)
+});
+
+app.get('/products-categories', (req, res) => {
+    res.send(categoryData);
+});
+
+app.listen(port, () => {
+    console.log(`Server Started On Port ${port}`);
+});
