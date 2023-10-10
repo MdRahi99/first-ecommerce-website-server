0452e3f6513c275c5ef8728cd3a7cf3477b896fe
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-10-10 20:23:56 +0600

    fix port address

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
