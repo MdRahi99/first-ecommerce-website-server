b3c4310e77338463e07871f5542f17d489b5cafb
MD Rahi <fhrstation21@gmail.com>
2023-11-27 01:04:57 +0600

    ...

--- index.js
+++ index.js
@@ -53,9 +53,19 @@ async function run() {
       res.send({token})
     });
 
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    });
+
     app.post('/users', async(req, res) => {
-      const users = req.body;
-      const result = await usersCollection.insertOne(users);
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
       res.send(result)
     });
 
